e12779f760616b10e37f429bf4e0f1f68f0c0464
Rarysson Guilherme <r_guilherme12@hotmail.com>
2020-07-17 11:50:37 -0300

    Adicionado login automático caso dê erro de autenticação

--- src/util/api.js
+++ src/util/api.js
@@ -3,8 +3,28 @@ import axios from "axios";
 const API = axios.create({
     baseURL: "https://aiko-estagio-proxy.azurewebsites.net/",
     params: {
-        token: process.env.TOKEN
+        token: process.env.VUE_APP_TOKEN
     }
 });
+
+API.get = async function(route, params = {}, repeat = true) {
+    try {
+        return await axios.get(API.defaults.baseURL + route, params);
+    } catch (error) {
+        const code = error.response.status;
+
+        if (code === 401 && repeat) {
+            await API.post("Login/Autenticar", null, {
+                params: {
+                    token: process.env.VUE_APP_TOKEN
+                }
+            });
+
+            return API.get(route, params, false);
+        } else {
+            throw error;
+        }
+    }
+};
 
 export default API;
